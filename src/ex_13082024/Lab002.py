8680597f4f04f4bb3dd80b054887d6bd429faa1e
SalaiKalaimamani <salaiaws1220@gmail.com>
2024-08-15 08:13:58 +0530

    Lab002

--- src/ex_13082024/Lab002.py
+++ src/ex_13082024/Lab002.py
@@ -1,4 +1,42 @@
-print(2+2)
-print(4/2) # division is always in Decimal
-print(4*2)
-print(4-2)
+# print(2+2)
+# print(4/2) # division is always in Decimal
+# print(4*2)
+# print(4-2)
+# res = max(5, 6)
+# # print(res)
+# # world = "Salai"
+# # print(type(world))
+# # min()
+#
+# pi = 3.14
+# res = pi + 1
+# print(res)
+
+# name = input("PLease enter you Name : ")
+# print (name)
+# print(type(name))
+
+# Num1 = int(input("Please enter the Value 1 :"))
+# Num2 = int(input("Please enter the Value 2 :"))
+# print("Addition ", Num1 + Num2)
+# print("Subtraction ", Num1 - Num2)
+# print("Multiplication ", Num1 * Num2)
+# print("Division", Num1 / Num2)
+
+
+# num=4.32323456787345345
+# demo=f"{num:.3f}"
+# print("what is the value for " F"{demo}")
+
+# multiplication Program using Formatting
+tab = 12
+print(f"{tab} * 1 = {tab * 1}")
+print(f"{tab} * 2 = {tab * 2}")
+print(f"{tab} * 3 ={tab * 3}")
+print(f"{tab} * 4 ={tab * 4}")
+print(f"{tab} * 5 ={tab * 5}")
+print(f"{tab} * 6 ={tab * 6}")
+print(f"{tab} * 7 ={tab * 7}")
+print(f"{tab} * 8 ={tab * 8}")
+print(f"{tab} * 9 ={tab * 9}")
+print(f"{tab} * 10 = {tab * 10}")
