685ca66360aa210c20fe32bf5d7e88ba625cf02a
SalaiKalaimamani <salaiaws1220@gmail.com>
2024-08-19 19:13:35 +0530

    Lab002

---
+++ src/ex_17082024/Task1.py
@@ -0,0 +1,21 @@
+# Taks 3: diff between = operator and == operator:
+# = Operator is used for assignment operations. EX: a = 10.
+# == Operator is used for comparsion operations. EX : a==10
+a = 10
+b = 20
+print(a == b)
+
+#What does the ** operator do in Python
+   # it will do Exponentiation multiplication
+# EX:
+print(4**5)
+
+#What does the ^ operator do in Python
+#compares each bit and set it to 1 if only one is 1, otherwise (if both are 1 or both are 0) it is set to 0:
+# 6 = 0000000000000110
+# 3 = 0000000000000011
+# --------------------
+# 5 = 0000000000000101
+# ====================
+
+print (6^3)
---
+++ src/ex_17082024/Task456.py
@@ -0,0 +1,23 @@
+# Task 4
+# Python program to calculate the area of a circle given its radius using the formula ``` area=π×r^2
+# take pi= 3.14
+# import math
+#
+# area = int(input("Enter the Radius Value:"))
+# Solution = (math.pi * (area ** 2))
+# print(Solution)
+
+# Task 5
+# program that takes two numbers as input and prints whether the first number is greater than, less than, or equal to the second number.
+# a = int(input("Enter the value 1 :"))
+# b = int(input("Enter the value 2 :"))
+#
+# print(a <= b)
+# print(a >= b)
+# print(a == b)
+#
+# # Task 6 : Python script that calculates the square and cube of a given number
+# s1 = int(input("Enter the value 1 :"))
+# c1 = int(input("Enter the value 2 :"))
+# print("Suare value of s1", s1 ** 2)
+# print("Cube value of c1 ", c1 ** 3)
